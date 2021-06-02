3fb0eeadd7a17484b651014fba67c5815b0d5257
mariangz <marianosgimenez@gmail.com>
2021-06-02 20:52:51 +0200

    improve navbar, add logo, edit tab size

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const path = require('path');
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath));
+app.use(express.static(publicPath));
 
 app.listen(3000, (req, res) => {
-    console.log('servidor corriendo en el puerto 3000');
+  console.log('servidor corriendo en el puerto 3000');
 });
 
 app.get('/', (req, res) => {
-    const publicHtml = path.resolve(__dirname, './views/home.html');
-    res.sendFile(publicHtml); 
+  const publicHtml = path.resolve(__dirname, './views/home.html');
+  res.sendFile(publicHtml);
 });
