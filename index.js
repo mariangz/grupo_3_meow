176cf630895b322702e69cc34261ddc83f683fe5
mariangz <marianosgimenez@gmail.com>
2021-06-03 13:52:05 +0200

    add register, login, product pages#

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
+// express
 const express = require('express');
-
 const path = require('path');
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
+// templates
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+// servidor
 app.listen(3000, (req, res) => {
   console.log('servidor corriendo en el puerto 3000');
 });
 
+// renderizar templates
 app.get('/', (req, res) => {
-  const publicHtml = path.resolve(__dirname, './views/home.html');
-  res.sendFile(publicHtml);
+  res.render('home');
 });
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/registro', (req, res) => {
+  res.render('register');
+});
+
+app.get('/producto', (req, res) => {
+  res.render('product');
+});
+
