d0948d524e304a221d9210aeb58a6f937b9a0bf2
mariangz <marianosgimenez@gmail.com>
2021-06-08 21:46:24 +0200

    fix style

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-// templates
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-
 // servidor
 app.listen(3000, (req, res) => {
   console.log('servidor corriendo en el puerto 3000');
 });
+
+// template engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 // renderizar templates
 app.get('/', (req, res) => {
