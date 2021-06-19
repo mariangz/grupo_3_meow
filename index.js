ae7199ce97499c259aaaaa3b9f70efc6623bf117
Mariano Giménez <marianosgimenez@gmail.com>
2021-06-19 14:58:21 +0200

    update index

--- index.js
+++ index.js
@@ -9,19 +9,20 @@ const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 // servidor
+// eslint-disable-next-line no-unused-vars
 app.listen(3000, (req, res) => {
+  // eslint-disable-next-line no-console
   console.log('servidor corriendo en el puerto 3000');
 });
 
 // template engine
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 
 // renderizar templates
 app.get('/', (req, res) => {
   res.render('home');
 });
-
 app.get('/login', (req, res) => {
   res.render('login');
 });
