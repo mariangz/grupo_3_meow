eab300741c987766356264d3ba75aa917cc23b95
Mariano Giménez <marianosgimenez@gmail.com>
2021-06-19 11:42:28 +0200

    add eslint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // express
 const express = require('express');
+
 const app = express();
 const path = require('path');
 
 // static files
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
-
 
 // servidor
 app.listen(3000, (req, res) => {
