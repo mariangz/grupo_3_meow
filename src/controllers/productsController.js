0445d56109a4e6fed727439940653d598caf29cb
ludmik79 <ludmik@gmail.com>
2021-07-13 22:28:30 -0300

    delete productos ejs, router y controller

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -14,18 +14,6 @@ const productsController = {
         res.render('products/productDetail', viewData);
     },
 
-    atun: (req, res) => res.render('products/atun'),
-
-    carne: (req, res) => res.render('products/carne'),
-
-    pollo: (req, res) => res.render('products/pollo'),
-
-    pescado: (req, res) => res.render('products/pescado'),
-
-    salmon: (req, res) => res.render('products/salmon'),
-
-    vegetariano: (req, res) => res.render('products/vegetariano'),
-
     crear: (req, res) => res.render('products/createProduct'),
 
     editar: (req, res) => res.render('products/editProduct'),
--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -4,12 +4,6 @@ const router = express.Router();
 
 const productsController = require('../controllers/productsController');
 
-router.get('/atun', productsController.atun);
-router.get('/carne', productsController.carne);
-router.get('/pescado', productsController.pescado);
-router.get('/salmon', productsController.salmon);
-router.get('/vegetariano', productsController.vegetariano);
-router.get('/pollo', productsController.pollo);
 router.get('/crear-productos', productsController.crear);
 router.get('/editar-productos', productsController.editar);
 router.get('/:id/', productsController.detalle);
