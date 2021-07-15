7d780a2f8f1262cc5e775f4394a1510f59eb3c55
ludmik79 <ludmik@gmail.com>
2021-07-15 19:49:35 -0300

    instalacion multer

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -1,8 +1,18 @@
 const express = require('express');
-
 const router = express.Router();
-
+const path = require('path');
+const multer = require('multer');
 const productsController = require('../controllers/productsController');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, '/public/images/food');
+    },
+    filename: function(req, file, cb) {
+        const imageProduct = file.originalname
+        cb(null, imageProduct);
+    }
+});
 
 router.get('/crear-productos', productsController.crear);
 router.get('/:id/editar-productos', productsController.editar); /* GET - formulario de edición de productos */
