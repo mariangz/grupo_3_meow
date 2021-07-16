d3400bd69365bdafd864cc5a38ef741c2380b231
ludmik79 <ludmik@gmail.com>
2021-07-15 22:01:43 -0300

    instalacion methodOverride

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 const express = require('express');
-
 const app = express();
+const methodOverride = require('method-override');
 const mainRouter = require('./routers/mainRouter');
 const productsRouter = require('./routers/productsRouter');
-const usersRouter= require('./routers/usersRouter');
+const usersRouter = require('./routers/usersRouter');
+
 
 app.use(express.static('../public'));
 
 app.set('view engine', 'ejs');
-
+app.use(methodOverride('_method'));
 app.use('/', mainRouter);
 app.use('/productos', productsRouter);
-app.use ('/usuarios', usersRouter);
+app.use('/usuarios', usersRouter);
+
 
 // eslint-disable-next-line no-console
 app.listen(3000, () => console.log('servidor en puerto 3000'));
