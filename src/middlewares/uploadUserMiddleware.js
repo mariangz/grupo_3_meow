f89bb8ac5c477bfe008cb0d4bd449d1e0353ac4e
ludmik79 <ludmik@gmail.com>
2021-07-29 21:06:24 -0300

    middleware para multer user

---
+++ src/middlewares/uploadUserMiddleware.js
@@ -0,0 +1,15 @@
+const multer = require('multer');
+
+/* Indicamos para subir el archivo nombre y donde guardarlo */
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, '../public/images/users');
+    },
+    filename(req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,32 +1,24 @@
 const express = require('express');
 
 const router = express.Router();
+
+// Controller
 const { body } = require('express-validator');
-const multer = require('multer');
 const usersController = require('../controllers/usersController');
+
+// Middlewares
+const upload = require('../middlewares/uploadUserMiddleware');
 
 // localhost:3000/users/registro
 router.get('/users/registro', usersController.registrar);
 
-/* Indicamos para subir el archivo nombre y donde guardarlo */
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, '../public/images/users');
-  },
-  filename(req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage });
-
 router.post('/users/registro',
 
-  body('email').isEmail().withMessage('Debe ser un email válido'),
-  body('password').isLength({ min: 6 }).withMessage('Debe tener al menos 6 caracteres'),
-  body('name').isString().isLength({ min: 2 }).withMessage('Debe tener al menos 2 caracteres'),
+    body('email').isEmail().withMessage('Debe ser un email válido'),
+    body('password').isLength({ min: 6 }).withMessage('Debe tener al menos 6 caracteres'),
+    body('name').isString().isLength({ min: 2 }).withMessage('Debe tener al menos 2 caracteres'),
 
-  usersController.guardarRegistro);
+    usersController.guardarRegistro);
 
 router.get('/users/login', usersController.ingresar);
 router.post('/users/login', usersController.guardarLogin);
