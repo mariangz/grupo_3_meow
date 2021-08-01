7cdaa3b341e1ca19fd5bcc65e518336241a5ec2c
ludmik79 <ludmik@gmail.com>
2021-07-31 23:07:01 -0300

    validaciones en LOGIN OK

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -2,9 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-
 // Controller
-const { body } = require('express-validator');
 const usersController = require('../controllers/usersController');
 
 // Middlewares
@@ -13,24 +11,22 @@ const validations = require('../middlewares/registerValidationMiddleware');
 const guestMiddleware = require('../middlewares/guestMiddleware');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-
-
 // Formulario de REGISTRO
 router.get('/register', guestMiddleware, usersController.register);
 
-// Procesar el REGISTRO
+// Proceso de REGISTRO
 router.post('/register', upload.single('image'), validations, usersController.processRegister);
 
 // Formulario de LOGIN
 router.get('/login', guestMiddleware, usersController.login);
 
 // Proceso de LOGIN
-router.post('/login', usersController.loginProcess);
+router.post('/login', validations, usersController.loginProcess);
 
-// Perfil de Usuario
+// Perfil USUARIO
 router.get('/profile', authMiddleware, usersController.profile);
 
-// Logout
+// Logout USUARIO
 router.get('/logout', usersController.logout);
 
 module.exports = router;
