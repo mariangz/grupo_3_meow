31fd4435b62c7cd02289135639905fd50bd7cc60
ludmik79 <ludmik@gmail.com>
2021-08-01 22:24:28 -0300

    userLoggedMiddleware OK a nivel APP en NAVBAR menu

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -4,13 +4,12 @@ const router = express.Router();
 
 // Middlewares
 const upload = require('../middlewares/uploadProductMiddleware');
-const authMiddleware = require('../middlewares/authMiddleware');
 
 // Controller
 const productsController = require('../controllers/productsController');
 
-router.get('/administrar', authMiddleware, productsController.listar); /* GET - listado de productos para administrar */
-router.get('/crear-productos', authMiddleware, productsController.crear); /* GET - formulario de creación de productos */
+router.get('/administrar', productsController.listar); /* GET - listado de productos para administrar */
+router.get('/crear-productos', productsController.crear); /* GET - formulario de creación de productos */
 router.post('/crear-productos', upload.single('image'), productsController.guardar); /* POST - Acción de edición a donde se envia el formulario */
 router.get('/editar-productos/:id', productsController.editar); /* GET - formulario de edición de productos */
 router.put('/editar-productos/:id', productsController.actualizar); /* PUT - Acción de edición a donde se envia el formulario */
