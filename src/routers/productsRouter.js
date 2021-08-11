83dcab67e3881002378f8635592e142292a9eb81
ludmik79 <ludmik@gmail.com>
2021-08-11 08:25:09 -0300

    createProduct funcionando

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -10,11 +10,11 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const productsController = require('../controllers/productsController');
 
 router.get('/adminProduct', authMiddleware, productsController.list); /* GET - listado de productos para administrar */
-router.get('/:id/', productsController.detail); /* GET - dellate de un producto en particular */
 router.get('/createProduct', authMiddleware, productsController.create); /* GET - formulario de creación de productos */
 router.post('/createProduct', upload.single('image'), productsController.save); /* POST - Acción de edición a donde se envia el formulario */
 router.get('/editProduct/:id', productsController.edit); /* GET - formulario de edición de productos */
 router.put('/editProduct/:id', productsController.update); /* PUT - Acción de edición a donde se envia el formulario */
 router.delete('/:id', productsController.delete); /* DELETE un producto */
+router.get('/:id/', productsController.detail); /* GET - dellate de un producto en particular */
 
 module.exports = router;
