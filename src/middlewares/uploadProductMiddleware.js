2759e3f202f5128b104390d8028fd361d75b1050
ludmik79 <ludmik@gmail.com>
2021-08-14 08:50:35 -0300

    require PATH delete from uploeadProductMiddleware

--- src/middlewares/uploadProductMiddleware.js
+++ src/middlewares/uploadProductMiddleware.js
@@ -1,14 +1,13 @@
-const path = require('path');
 const multer = require('multer');
 
 /* Indicamos para subir el archivo nombre y donde guardarlo */
 const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, '../public/images/food');
-  },
-  filename(req, file, cb) {
-    cb(null, file.originalname);
-  },
+    destination(req, file, cb) {
+        cb(null, '../public/images/food');
+    },
+    filename(req, file, cb) {
+        cb(null, file.originalname);
+    },
 });
 
 const upload = multer({ storage });
