5615fdb6caf609cb696d5c92f56dd1b318623385
ludmik79 <ludmik@gmail.com>
2021-08-20 07:59:05 -0300

    fix rutas middleware imagenes user y product

--- src/middlewares/uploadProductMiddleware.js
+++ src/middlewares/uploadProductMiddleware.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 /* Indicamos para subir el archivo nombre y donde guardarlo */
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, '../public/images/food');
+        cb(null, './public/images/food');
     },
     filename(req, file, cb) {
         cb(null, file.originalname);
--- src/middlewares/uploadUserMiddleware.js
+++ src/middlewares/uploadUserMiddleware.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 /* Indicamos para subir el archivo nombre y donde guardarlo */
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, '../public/images/users');
+        cb(null, './public/images/users');
     },
     filename(req, file, cb) {
         cb(null, file.originalname);
