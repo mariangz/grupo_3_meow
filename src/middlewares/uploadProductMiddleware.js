8b8868932280ff86c31fe25df51d8885f16ba9cc
mariangz <marianosgimenez@gmail.com>
2021-08-28 20:00:33 +0200

    update function

--- src/middlewares/uploadProductMiddleware.js
+++ src/middlewares/uploadProductMiddleware.js
@@ -3,7 +3,8 @@ const multer = require('multer');
 /* Indicamos para subir el archivo nombre y donde guardarlo */
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, './public/images/food');
+    const folder = './public/images/food';
+    cb(null, folder);
   },
   filename(req, file, cb) {
     cb(null, file.originalname);
