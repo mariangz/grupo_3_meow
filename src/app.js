0faec22f2ed27c72170184485b55ed9d835d5b58
mariangz <marianosgimenez@gmail.com>
2021-08-30 08:59:24 +0200

    format code

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const session = require("express-session");
-const cookies = require("cookie-parser");
-const methodOverride = require("method-override");
-const logger = require("morgan");
-const path = require("path");
+const express = require('express');
+const session = require('express-session');
+const cookies = require('cookie-parser');
+const methodOverride = require('method-override');
+const logger = require('morgan');
+const path = require('path');
 
 const app = express();
 
-const userLoggedMiddleware = require("./middlewares/userLoggedMiddleware");
+const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
 
 // Routers
-const mainRouter = require("./routers/mainRouter");
-const productsRouter = require("./routers/productsRouter");
-const usersRouter = require("./routers/usersRouter");
+const mainRouter = require('./routers/mainRouter');
+const productsRouter = require('./routers/productsRouter');
+const usersRouter = require('./routers/usersRouter');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(
   session({
     secret: "Shh, it's a secret",
@@ -27,16 +27,17 @@ app.use(userLoggedMiddleware);
 app.use(express.urlencoded({ extended: false }));
 
 // Template engine
-app.set("view engine", "ejs");
-app.set("views", path.resolve(__dirname, "./views"));
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, './views'));
 
 app.use(express.json());
-app.use(methodOverride("_method"));
-app.use(express.static(path.resolve("public")));
+app.use(methodOverride('_method'));
+app.use(express.static(path.resolve('public')));
 // eslint-disable-next-line no-console
-app.listen(3000, () => console.log("servidor en puerto 3000"));
+const server = 3000;
+app.listen(server, () => console.log(`servidor en puerto ${server}`));
 
-app.use("/", mainRouter);
-app.use("/products", productsRouter);
-app.use("/users", usersRouter);
-app.use((req, res) => res.status(404).render("404"));
+app.use('/', mainRouter);
+app.use('/products', productsRouter);
+app.use('/users', usersRouter);
+app.use((req, res) => res.status(404).render('404'));
