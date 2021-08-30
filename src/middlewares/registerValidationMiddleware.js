fbe81da4c43e652a8aae2f84c431c8eff8af02db
mariangz <marianosgimenez@gmail.com>
2021-08-30 08:17:43 +0200

    format code

--- src/middlewares/registerValidationMiddleware.js
+++ src/middlewares/registerValidationMiddleware.js
@@ -1,31 +1,31 @@
-const path = require("path");
-const { body } = require("express-validator");
+const path = require('path');
+const { body } = require('express-validator');
 
 // validación
 const validations = [
-  body("name").notEmpty().withMessage("Tienes que escribir un nombre"),
-  body("email")
+  body('name').notEmpty().withMessage('Tienes que escribir un nombre'),
+  body('email')
     .notEmpty()
-    .withMessage("Debes ingresar un correo electrónico")
+    .withMessage('Debes ingresar un correo electrónico')
     .bail()
     .isEmail()
-    .withMessage("Debes escribir un formato de correo válido"),
-  body("password").notEmpty().withMessage("Tienes que escribir una contraseña"),
-  body("confirmPassword")
+    .withMessage('Debes escribir un formato de correo válido'),
+  body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
+  body('confirmPassword')
     .notEmpty()
-    .withMessage("Debes confirmar la contraseña anterior"),
-  body("image").custom((value, { req }) => {
+    .withMessage('Debes confirmar la contraseña anterior'),
+  body('image').custom((value, { req }) => {
     const { file } = req;
-    const acceptedExtensions = [".jpg", ".png", ".gif"];
+    const acceptedExtensions = ['.jpg', '.png', '.gif'];
 
     if (!file) {
-      throw new Error("Tienes que subir una imagen");
+      throw new Error('Tienes que subir una imagen');
     } else {
       const fileExtension = path.extname(file.originalname);
       if (!acceptedExtensions.includes(fileExtension)) {
         throw new Error(
           `Las extensiones de archivo permitidas son ${acceptedExtensions.join(
-            " , "
+            ' , '
           )}`
         );
       }
