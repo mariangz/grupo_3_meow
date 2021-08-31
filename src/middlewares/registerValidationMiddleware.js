83b4344c5a023879af832fc06aea798cfbea60e8
mariangz <marianosgimenez@gmail.com>
2021-08-31 16:30:10 +0200

    update validations and form ejs

--- src/middlewares/registerValidationMiddleware.js
+++ src/middlewares/registerValidationMiddleware.js
@@ -24,7 +24,7 @@ const validations = [
       if (!acceptedExtensions.includes(fileExtension)) {
         throw new Error(
           `Las extensiones de archivo permitidas son ${acceptedExtensions.join(
-            ' , '
+            ','
           )}`
         );
       }
