5e1886b3a903c42681f4781d451d06d91de8ef01
mariangz <marianosgimenez@gmail.com>
2021-09-03 16:41:20 +0200

    update form reg

--- public/js/registerValidation.js
+++ public/js/registerValidation.js
@@ -5,24 +5,25 @@ const showError = (input) => {
   if (input.validity.valueMissing) {
     span.textContent = 'Campo obligatorio';
   } else if (input.validity.tooShort) {
-    span.textContent = `Al menos ${input.minLength} caracteres.`;
+    span.textContent = `Al menos ${input.minLength} caracteres`;
   } else if (input.validity.typeMismatch) {
     span.textContent = input.title;
   } else if (input.validity.patternMismatch) {
     span.textContent = input.title;
   }
-  span.classList.add = 'active';
+  span.classList.add('invalid');
+  input.classList.add('invalid');
 };
 
-form.addEventListener('input', (event) => {
-  const input = event.target;
-  if (input.validity.valid) {
-    input.classList.remove('invalid');
-    input.textContent = '';
-    input.className = 'error';
-  } else {
-    showError(input);
-  }
-});
+const hideError = (input) => {
+  const span = document.querySelector(`#${input.id} + span.error`);
+  span.textContent = '';
+  span.classList.remove('invalid');
+  input.classList.remove('invalid');
+};
 
-console.log(form);
+form.addEventListener('input', (event) =>
+  event.target.validity.valid
+    ? hideError(event.target)
+    : showError(event.target)
+);
