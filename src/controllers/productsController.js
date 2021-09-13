16922d8079619a53316fd80facdaa0a3248aee7b
ludmik79 <ludmik@gmail.com>
2021-09-13 08:30:34 -0300

    find product Op.sequelize

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,6 +1,8 @@
 // const fs = require('fs');
 // const path = require('path');
 const db = require('../database/models');
+
+const Op = db.Sequelize.Op;
 // const productsFilePath = path.join(__dirname, '../data/products.json');
 // const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
