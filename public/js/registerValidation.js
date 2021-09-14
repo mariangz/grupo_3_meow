dc9b5d630e7324675d2e4dd04a885fabae09f9ec
mariangz <marianosgimenez@gmail.com>
2021-09-14 14:32:07 +0200

    fix validation confirm password

--- public/js/registerValidation.js
+++ public/js/registerValidation.js
@@ -1,6 +1,5 @@
 const form = document.getElementById('register');
 const confirmPassword = document.getElementById('confirmar-contraseña');
-// const file = document.getElementById('userAddImage');
 
 const showError = (input) => {
   const span = document.querySelector(`#${input.id} + span.error`);
@@ -11,6 +10,8 @@ const showError = (input) => {
   } else if (input.validity.typeMismatch) {
     span.textContent = input.title;
   } else if (input.validity.patternMismatch) {
+    span.textContent = input.title;
+  } else if (input.value !== document.getElementById('password')) {
     span.textContent = input.title;
   }
   span.classList.add('invalid');
@@ -32,27 +33,16 @@ const hideError = (input) => {
   input.classList.add('success');
 };
 
-form.addEventListener('input', (event) =>
-  event.target.validity.valid
-    ? hideError(event.target)
-    : showError(event.target)
-);
-
-confirmPassword.addEventListener('input', () => {
+form.addEventListener('input', (event) => {
   const password = document.getElementById('password');
-  const icon = document.querySelector(`#confirmPassword ~ .input-icon`);
-  const span = document.querySelector(`#confirmPassword + span.error`);
-  if (confirmPassword.value !== password.value) {
-    span.classList.add('invalid');
-    icon.classList.add('input-icon--error', 'fa-exclamation-circle');
-    input.classList.remove('success');
-    input.classList.add('invalid');
-  } else {
-    icon.classList.remove('input-icon--error', 'fa-exclamation-circle');
-    icon.classList.add('input-icon--success', 'fa-check-circle');
-    confirmPassword.classList.remove('invalid');
-    confirmPassword.classList.add('success');
+  if (event.target.id === 'confirmar-contraseña') {
+    return event.target.value === password.value
+      ? hideError(event.target)
+      : showError(event.target);
   }
+  return event.target.validity.valid
+    ? hideError(event.target)
+    : showError(event.target);
 });
 
 // const imgValidation = () => {
