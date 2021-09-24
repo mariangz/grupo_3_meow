70f8d516a351e1d0f7cb96df6ba709b59ef5589e
ludmik79 <ludmik@gmail.com>
2021-09-23 23:16:52 -0300

    usuario vs administrador OK

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -7,14 +7,15 @@ const upload = require('../middlewares/uploadProductMiddleware');
 const authMiddleware = require('../middlewares/authMiddleware');
 const validationCreate = require('../middlewares/createProductMiddleware');
 const validationEdit = require('../middlewares/editProductMiddleware');
+const adminMiddleware = require('../middlewares/adminMiddleware');
 
 // Controller
 const productsController = require('../controllers/productsController');
 
-router.get('/adminProduct', authMiddleware, productsController.list); /* GET - listado de productos para administrar */
-router.get('/createProduct', authMiddleware, productsController.create); /* GET - formulario de creación de productos */
+router.get('/adminProduct', authMiddleware, adminMiddleware, productsController.list); /* GET - listado de productos para administrar */
+router.get('/createProduct', authMiddleware, adminMiddleware, productsController.create); /* GET - formulario de creación de productos */
 router.post('/createProduct', upload.single('image'), validationCreate, productsController.save); /* POST - Acción de edición a donde se envia el formulario */
-router.get('/editProduct/:id', productsController.edit); /* GET - formulario de edición de productos */
+router.get('/editProduct/:id', adminMiddleware, productsController.edit); /* GET - formulario de edición de productos */
 router.put('/editProduct/:id', validationEdit, productsController.update); /* PUT - Acción de edición a donde se envia el formulario */
 router.delete('/:id', productsController.delete); /* DELETE un producto */
 router.get('/:id/', productsController.detail); /* GET - dellate de un producto en particular */
