bd119d2ba1ce81369d9fa1830ea5e8f2b0e8cd1a
mariangz <marianosgimenez@gmail.com>
2021-09-24 13:50:41 +0200

    add login.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-    development: {
-        username: 'root',
-        password: null,
-        database: 'meow_db',
-        host: '127.0.0.1',
-        dialect: 'mysql',
-    },
-    test: {
-        username: 'root',
-        password: null,
-        database: 'database_test',
-        host: '127.0.0.1',
-        dialect: 'mysql',
-    },
-    production: {
-        username: 'root',
-        password: null,
-        database: 'database_production',
-        host: '127.0.0.1',
-        dialect: 'mysql',
-    },
+  development: {
+    username: 'root',
+    password: 'tejedor709',
+    database: 'meow_db',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
 };
