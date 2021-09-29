27bc3bc1f058467605fdf861a19f4c0196186b03
mariangz <marianosgimenez@gmail.com>
2021-09-29 16:03:06 +0200

    add validation en todos los campos

--- public/js/productValidation.js
+++ public/js/productValidation.js
@@ -1,49 +1,79 @@
 const form = document.querySelector('form');
-console.log(form);
-const name = document.getElementById('name');
+const form = document.querySelector('.field');
+const nombre = document.getElementById('name');
+const price = document.getElementById('price');
 const description = document.getElementById('description');
 const nutritional = document.getElementById('nutritional');
+const radios = document.querySelectorAll('input[type="radio"]');
+console.log(radio);
+console.log(nutritional);
 
 const showError = (input) => {
-  const icon = document.querySelector(`#${input.id} ~ .input-icon`);
+  console.log(input);
   const span = document.querySelector(`#${input.id} + span.error`);
   if (input.validity.valueMissing) {
     span.textContent = 'Campo obligatorio';
   } else if (input.validity.tooShort) {
     span.textContent = `Al menos ${input.minLength} caracteres`;
-  } else if (input.validity.typeMismatch) {
-    span.textContent = input.title;
-  } else if (input.validity.patternMismatch) {
-    span.textContent = input.title;
   }
+
   span.classList.add('invalid');
   input.classList.add('invalid');
   input.classList.remove('success');
-  icon.classList.add('input-icon--error', 'fa-exclamation-circle');
 };
 
 const hideError = (input) => {
   const span = document.querySelector(`#${input.id} + span.error`);
-  if (input.name === 'image') {
-    span.textContent = '';
-    span.classList.remove('invalid');
-    input.classList.remove('invalid');
-    input.classList.add('success');
-  } else {
-    span.textContent = '';
-    span.classList.remove('invalid');
-    input.classList.remove('invalid');
-    const icon = document.querySelector(`#${input.id} ~ .input-icon`);
-    icon.classList.remove('input-icon--error', 'fa-exclamation-circle');
-    icon.classList.add('input-icon--success', 'fa-check-circle');
-    input.classList.add('success');
-  }
+  span.textContent = '';
+  span.classList.remove('invalid');
+  input.classList.remove('invalid');
+  input.classList.add('success');
 };
 
-form.addEventListener('input', (event) => {
-  if (event.target.validity.valid) {
-    hideError(event.target);
-  } else {
-    showError(event.target);
+nutritional.addEventListener('input', (event) => console.log(event.target));
+
+let radio = false;
+for (let e of radios) {
+  if (e.check === true) {
+    radio = true;
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  const image = document.getElementById('productAddImage');
+  const fileExt = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+
+  if (!nombre.validity.valid) {
+    event.preventDefault();
+    showError(nombre);
+  }
+  if (!price.value.valid) {
+    event.preventDefault();
+    showError(price);
+  }
+  if (
+    description.textContent.trim() === '' ||
+    nutritional.textContent.trim().length > 20
+  ) {
+    event.preventDefault();
+    showError(nutritional);
+  }
+
+  if (
+    nutritional.textContent.trim() === '' ||
+    nutritional.textContent.trim().length > 20
+  ) {
+    event.preventDefault();
+    showError(nutritional);
+  }
+
+  if (!radio) {
+    event.preventDefault();
+    showError(field);
+  }
+
+  if (!fileExt.exec(image.value)) {
+    event.preventDefault();
+    showError(image);
   }
 });
