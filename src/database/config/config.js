d90e01dbbb74575a8f5bdd4d83c2904e3205b6b8
mariangz <marianosgimenez@gmail.com>
2021-09-29 21:30:37 +0200

    add edit product validation

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: 'root',
-    password: 'tejedor709',
+    password: null,
     database: 'meow_db',
     host: '127.0.0.1',
     dialect: 'mysql',
